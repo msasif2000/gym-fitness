137a1726c8542f45425576cec3b45a72d0c2054c
Asif <msasifiiuc@gmail.com>
2023-09-18 23:02:22 +0600

    chart added but not responsive in for small devices

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import Navbar from './assets/component/Navbar/Navbar'
 import PriceOptions from './assets/component/PriceOptions/PriceOptions'
 import LineChart from './assets/component/LineChart/LineChart'
-import DataFetch from './assets/component/DataFetch/DataFetch'
+
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
         <Navbar></Navbar>
         <PriceOptions></PriceOptions>
         <LineChart></LineChart>
-        <DataFetch></DataFetch>
+
       </div>
     </>
   )
